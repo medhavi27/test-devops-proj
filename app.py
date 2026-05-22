aafdccdeeb6027ae7f8b7a033f4f92716ed1ac62
Medhavi Gandhi <mg2252@cornell.edu>
2026-05-22 17:15:05 +0100

    move init

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ from routes import api
 
 app = Flask(__name__)
 
-# 1. Fire up the database connection loop & table schemas
-init_db()
-
 # 2. Register our routes Blueprint with the main Flask app
 app.register_blueprint(api)
 
 if __name__ == "__main__":
+    # 1. Fire up the database connection loop & table schemas
+    init_db()
     app.run(host="0.0.0.0", port=5000)
